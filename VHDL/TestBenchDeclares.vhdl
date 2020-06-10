30b1eb38770224fed0d2267e4da9adb1842cf15a
Jay Jaeger <cube1us@gmail.com>
2020-06-10 19:24:05 +0000

    Added ALDs for 11.10.2%.1 Added TestBenchDeclares.vhdl
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@1802 8de05751-f04e-1843-ac3c-a4bcb19ff523

---
+++ VHDL/TestBenchDeclares.vhdl
@@ -0,0 +1,13 @@
+-- START USER TEST BENCH DECLARATIONS
+
+-- The user test bench declaraionts, if any, must be
+-- placed AFTER the line starts with the first line of text 
+-- that -- START USER TEST BENCH DECLARATIONS and ends
+-- with --END .
+-- This text is preserved when the IBM1410SMS applciation
+-- regenerates a test bench
+
+   -- Your test bench declarations go here
+
+-- END USER TEST BENCH DECLARATIONS
+   
