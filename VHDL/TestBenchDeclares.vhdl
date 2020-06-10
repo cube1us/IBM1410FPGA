01e4a355ef42598120562987e5aa168ac9316efd
Jay Jaeger <cube1us@gmail.com>
2020-06-10 22:16:27 +0000

    Added 11.10.2%.1 to the LogicGateRing.vhdl and associated test bench. Cosmetic edits to the test bench delcaration area template.
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@1803 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/TestBenchDeclares.vhdl
+++ VHDL/TestBenchDeclares.vhdl
@@ -1,9 +1,9 @@
 -- START USER TEST BENCH DECLARATIONS
 
--- The user test bench declaraionts, if any, must be
+-- The user test bench declarations, if any, must be
 -- placed AFTER the line starts with the first line of text 
--- that -- START USER TEST BENCH DECLARATIONS and ends
--- with --END .
+-- with -- START USER TEST BENCH DECLARATIONS and ends
+-- with -- END USER TEST BENCH DECLARATIONS
 -- This text is preserved when the IBM1410SMS applciation
 -- regenerates a test bench
 
