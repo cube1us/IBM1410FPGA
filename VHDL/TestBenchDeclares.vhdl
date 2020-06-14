28a5b7c711dfc4536288f358f8e6c0b186616a31
Jay Jaeger <cube1us@gmail.com>
2020-06-14 17:31:24 +0000

    TestBenchDeclares.vhdl: Fix comment so it doesn't trip end of copied declarations area
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@1813 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/TestBenchDeclares.vhdl
+++ VHDL/TestBenchDeclares.vhdl
@@ -3,7 +3,7 @@
 -- The user test bench declarations, if any, must be
 -- placed AFTER the line starts with the first line of text 
 -- with -- START USER TEST BENCH DECLARATIONS and ends
--- with -- END USER TEST BENCH DECLARATIONS
+-- with the line containing -- END (and the rest of the line) below.
 -- This text is preserved when the IBM1410SMS applciation
 -- regenerates a test bench
 
