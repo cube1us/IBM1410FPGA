cf7130a20c769a307f98022fe3f280e1bb5be474
Jay Jaeger <cube1us@gmail.com>
2020-06-28 19:27:55 +0000

    TestBenchDeclares.vhdl:  Add bit ID constants and check1 procedure to test bench declarations.
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@1859 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/TestBenchDeclares.vhdl
+++ VHDL/TestBenchDeclares.vhdl
@@ -7,6 +7,26 @@
 -- This text is preserved when the IBM1410SMS applciation
 -- regenerates a test bench
 
+   constant HDL_C_BIT: integer := 7;
+   constant HDL_WM_BIT: integer := 6;
+   constant HDL_B_BIT: integer := 5;
+   constant HDL_A_BIT: integer := 4;
+   constant HDL_8_BIT: integer := 3;
+   constant HDL_4_BIT: integer := 2;
+   constant HDL_2_BIT: integer := 1;
+   constant HDL_1_BIT: integer := 0;
+
+procedure check1(
+    checked: in STD_LOGIC;
+    val: in STD_LOGIC;
+    testname: in string;
+    test: in string) is
+    begin    
+    assert checked = val report testname & " (" & test & ") failed." severity failure;
+    end procedure;
+      
+
+
    -- Your test bench declarations go here
 
 -- END USER TEST BENCH DECLARATIONS
