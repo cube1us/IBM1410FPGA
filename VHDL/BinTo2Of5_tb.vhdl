fcf76c2eeb17dcade54045d3b43465a36cc55f39
Jay Jaeger <cube1us@gmail.com>
2020-08-27 21:26:28 +0000

    Pages 14.18.11.1 - 16.1 Group BinTo2Of5 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2131 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/BinTo2Of5_tb.vhdl
+++ VHDL/BinTo2Of5_tb.vhdl
@@ -207,6 +207,112 @@ uut_process: process
    begin
 
    -- Your test bench code
+   
+   testName := "14.18.11.1, 12.1  ";
+
+   for aBit in std_logic range '0' to '1' loop
+      PS_ASSEMBLY_CH_BUS(HDL_A_BIT) <= aBit;
+      MS_ASSEMBLY_CH_BUS(HDL_A_BIT) <= NOT aBit;
+      for bBit in std_logic range '0' to '1' loop
+         PS_ASSEMBLY_CH_BUS(HDL_B_BIT) <= bBit;
+         MS_ASSEMBLY_CH_BUS(HDL_B_BIT) <= NOT bBit;
+            for bit8 in std_logic range '0' to '1' loop
+               PS_AUX_BIN_ADDER_8_BIT <= bit8;
+               MS_AUX_BIN_ADDER_8_BIT <= NOT bit8;
+               for bit4 in std_logic range '0' to '1' loop
+                  PS_AUX_BIN_ADDER_4_BIT <= bit4;
+                  MS_AUX_BIN_ADDER_4_BIT <= NOT bit4;
+                  
+                  PS_TSLT_BINARY_TO_TH_POS_1 <= '0';
+                  PS_TSLT_BINARY_TO_TH_POS_2 <= '0';
+                  PS_TSLT_BINARY_TO_TTH_POS <= '0';
+                  
+                  wait for 30 ns;
+                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_A,'1',testName,"0A");
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_B,'1',testName,"0B");
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_C,'1',testName,"0C");
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_D,'1',testName,"0D");
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_E,'1',testName,"0E");
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_F,'1',testName,"0F");
+                  check1(MS_COMPAT_TSLTR_THP_4_BIT_A,'1',testName,"4A");
+                  check1(MS_COMPAT_TSLTR_THP_4_BIT_B,'1',testName,"4B");
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_A,'1',testName,"1A");
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_B,'1',testName,"1B");
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_C,'1',testName,"1C");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_A,'1',testName,"2A");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_B,'1',testName,"2B");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_C,'1',testName,"2C");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_D,'1',testName,"2D");
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_A,'1',testName,"8A");
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_B,'1',testName,"8B");
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_C,'1',testName,"8C");
+                  
+                  check1(MS_COMPAT_TSLTR_THP_2_DOT_8_BITS,'1',testName,"AA");
+                                    
+                  PS_TSLT_BINARY_TO_TH_POS_1 <= '1';
+                  PS_TSLT_BINARY_TO_TH_POS_2 <= '1';                  
+                  
+                  wait for 30 ns;
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_A,
+                     NOT(bBit and not bit8 and not bit4 and not aBit),testName,"0AA");                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_B,
+                     NOT(abit and not bit8 and not bit4 and not bbit),testName,"0BA");                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_C,
+                     NOT(abit and bit8 and bbit and not bit4),testName,"0CA");                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_D,
+                     NOT(bit4 and bit8 and not abit),testName,"0DA");                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_E,
+                     NOT(bit8 and not bbit and not abit),testName,"0EA");                  
+                  check1(MS_COMPAT_TSLTR_THP_0_BIT_F,
+                     NOT(bit4 and not bbit and not abit),testName,"0FA");
+                     
+                  check1(MS_COMPAT_TSLTR_THP_4_BIT_A,NOT(not bit8 and bit4),testName,"4AA");                  
+                  check1(MS_COMPAT_TSLTR_THP_4_BIT_B,NOT(bit4 and bbit),testName,"4BA");
+                  
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_A,NOT(not bit4 and abit),testName,"1AA");
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_B,NOT(abit and not bbit),testName,"1BA");
+                  check1(MS_COMPAT_TSLTR_THP_1_BIT_C,NOT(abit and bit8),testName,"1CA");
+                  
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_A,NOT(bit8 and bit4 and not bbit),testName,"2AA");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_B,
+                    NOT(not bit8 and bbit and not abit),testName,"2BA");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_C,
+                    NOT(not bit8 and not abit and not bit4),testName,"2CA");
+                  check1(MS_COMPAT_TSLTR_THP_2_BIT_D,NOT(not bit8 and not bit4 and bbit),
+                    testName,"2BD");
+
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_A,
+                     NOT(bit8 and not bit4 and not bbit),testName,"8AA");
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_B,
+                        NOT(not bbit and not bit4 and not abit),testName,"8BA");
+                  check1(MS_COMPAT_TSLTR_THP_8_BIT_C,
+                           NOT(bit4 and bbit and abit and not bit8),testName,"8CA");
+
+                  check1(MS_COMPAT_TSLTR_THP_2_DOT_8_BITS,
+                           NOT(bit8 and bbit and not bit4 and not abit),testName,"AAA");
+                           
+                  check1(MS_COMPAT_TSLTR_TTHP_2_DOT_8_BITS_A,'1',testName,"2.8A A");
+                  check1(MS_COMPAT_TSLTR_TTHP_2_DOT_8_BITS_B,'1',testName,"2.8B A");
+                  check1(MS_COMPAT_TSLTR_TTHP_0_DOT_1_BITS_A,'1',testName,"0.1A A");
+                  check1(MS_COMPAT_TSLTR_TTHP_0_DOT_1_BITS_B,'1',testName,"0.1B A");
+
+                  PS_TSLT_BINARY_TO_TTH_POS <= '1';
+                  wait for 30 ns;
+                  
+                  check1(MS_COMPAT_TSLTR_TTHP_2_DOT_8_BITS_A,
+                     NOT(not bit4 and not bbit),testName,"2.8A B");
+                  check1(MS_COMPAT_TSLTR_TTHP_2_DOT_8_BITS_B,
+                     NOT(not bit8),testName,"2.8B B");
+                  check1(MS_COMPAT_TSLTR_TTHP_0_DOT_1_BITS_A,
+                     NOT(bit4 and bit8),testName,"0.1A B");
+                  check1(MS_COMPAT_TSLTR_TTHP_0_DOT_1_BITS_B,
+                     NOT(bit8 and bbit),testName,"0.1B B");                                    
+                                       
+               end loop;               
+            end loop;
+      end loop;
+   end loop;   
 
    wait;
    end process;
