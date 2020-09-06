fe3c7768e76ac35cfe5804fbafcc5b03c592fcf6
Jay Jaeger <cube1us@gmail.com>
2020-09-06 15:39:28 +0000

    Page 14.71.24.1 - fix test name
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2207 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_14_71_24_1_I_AR_RESET_tb.vhdl
+++ VHDL/ALD_14_71_24_1_I_AR_RESET_tb.vhdl
@@ -160,7 +160,7 @@ uut_process: process
    begin
 
    -- Your test bench code
-   testName := "14.71.23.1        ";
+   testName := "14.71.24.1        ";
 
    for t in 0 to 8192 loop
       tv := std_logic_vector(to_unsigned(t,tv'Length));
