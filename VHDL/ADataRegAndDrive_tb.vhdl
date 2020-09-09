4d176b4c98afc9b709f8b3294b86c56d0ca020af
Jay Jaeger <cube1us@gmail.com>
2020-09-09 21:26:16 +0000

    Pages 15.39.*.1 Group ADataRegAndDrive: Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2253 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ADataRegAndDrive_tb.vhdl
+++ VHDL/ADataRegAndDrive_tb.vhdl
@@ -169,17 +169,223 @@ fpga_clk_process: process
 
 -- Place your test bench code in the uut_process
 
+
+-- Continuous assignment.
+
+PS_B_CH_NOT_BUS <= NOT PS_B_CH_BUS;
+
 uut_process: process
 
    variable testName: string(1 to 18);
    variable subtest: integer;
-   variable tv: std_logic_vector(15 downto 0);
+   variable tv: std_logic_vector(7 downto 0);
+   variable tv2: std_logic_vector(7 downto 0);
    variable a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p: std_logic;
    variable g1, g2, g3, g4, g5, g6: std_logic;
+   
 
    begin
 
    -- Your test bench code
+   
+   testName := "A Data Reg & Drive";
+   
+   MS_RESET_A_DATA_REG <= '0';
+   wait for 30 ns;
+   MS_RESET_A_DATA_REG <= '1';
+   wait for 30 ns;
+
+   for b in 0 to 7 loop
+      if(b /= 7) then
+         check1(PS_A_DATA_REG_BUS(b),'0',testName,"1A BIT " & INTEGER'Image(b));
+      else -- Reset SETS check bit
+         check1(PS_A_DATA_REG_BUS(b),'1',testName,"1A C BIT " & INTEGER'Image(b));
+      end if;
+   end loop;
+   
+   
+   for t1 in 0 to 255 loop
+   
+      MS_RESET_A_DATA_REG <= '0';
+      wait for 30 ns;
+      MS_RESET_A_DATA_REG <= '1';
+      wait for 30 ns;
+      
+      tv := std_logic_vector(to_unsigned(t1,tv'Length));
+      
+      PS_SW_AR_EXIT_CH_TO_A_REG <= '0';
+      PS_AR_EXIT_CH_BUS <= "00000000";      
+      PS_SW_B_CH_TO_A_REG <= '1';      
+      PS_B_CH_BUS <= tv;
+      PS_B_CH_WM_BIT_1 <= tv(6);      
+      wait for 30 ns;  -- Set appropriate bits
+      PS_SW_B_CH_TO_A_REG <= '0';      
+      PS_B_CH_BUS <= "00000000";
+      PS_B_CH_WM_BIT_1 <= '0';
+      wait for 30 ns; -- Should stay set
+      
+      for bbit in 0 to 7 loop            
+            check1(PS_A_DATA_REG_BUS(bbit),tv(bbit),testName,"1B BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_BUS(bbit),'0',testName,"1C BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),'1',testName,"1D BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),'0',testName,"1E BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),'1',testName,"1F BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+      
+      PS_GATE_A_DATA_REG_TO_A_CH <= '1';
+      wait for 30 ns;
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),tv(bbit),testName,"1G BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"1H BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),tv(bbit),testName,"1I BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"1J BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+      PS_GATE_A_DATA_REG_TO_A_CH <= '0';
+      
+      
+      MS_RESET_A_DATA_REG <= '0';
+      wait for 30 ns;
+      MS_RESET_A_DATA_REG <= '1';
+      wait for 30 ns;
+      
+      tv := not tv;   -- Invert test vector to be certain of no crossover       
+
+      PS_SW_AR_EXIT_CH_TO_A_REG <= '1';
+      PS_AR_CH_STAR_TRANSLATOR_STAR_BUS(2) <= tv(2); -- 4 bit is handled specially
+      PS_AR_EXIT_CH_BUS <= tv;      
+      PS_SW_B_CH_TO_A_REG <= '0';      
+      PS_B_CH_BUS <= "00000000";
+      PS_B_CH_WM_BIT_1 <= '0';      
+      wait for 30 ns;  -- Set appropriate bits
+      PS_SW_AR_EXIT_CH_TO_A_REG <= '0';
+      PS_AR_EXIT_CH_BUS <= "00000000";      
+      wait for 30 ns; -- Should stay set
+      
+      for bbit in 0 to 7 loop -- Does not set WM, B or A bits, and 4 bit comes from "translator"
+         if(bbit < 4 or bbit = 7) then            
+            check1(PS_A_DATA_REG_BUS(bbit),tv(bbit),testName,"1K BIT " & INTEGER'Image(bbit));
+         end if;
+      end loop;
+
+   end loop;
+
+   PS_F2_REG_BUS <= "10101010";
+   PS_OP_MOD_REG_BUS <= "01010101";
+   
+   for t1 in 0 to 255 loop
+      tv := std_logic_vector(to_unsigned(t1,tv'Length));
+      PS_GATE_E2_DATA_REG_TO_A_CH <= '0';
+      PS_E2_REG_BUS <= tv;
+      wait for 30 ns;
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),'0',testName,"2A BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),'1',testName,"2B BIT " & INTEGER'Image(bbit));
+            check1(LAMPS_A_CH(bbit),'0',testName,"2C BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),'0',testName,"2D BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),'1',testName,"2E BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+
+      PS_GATE_E2_DATA_REG_TO_A_CH <= '1';
+      wait for 30 ns;      
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),tv(bbit),testName,"2F BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"2G BIT " & INTEGER'Image(bbit));
+            check1(LAMPS_A_CH(bbit),tv(bbit),testName,"2H BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),tv(bbit),testName,"2I BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"2J BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+   end loop;
+
+   PS_GATE_E2_DATA_REG_TO_A_CH <= '0';
+   PS_E2_REG_BUS <= "10101010";
+   PS_OP_MOD_REG_BUS <= "01010101";
+   
+   for t1 in 0 to 255 loop
+      tv := std_logic_vector(to_unsigned(t1,tv'Length));
+      PS_GATE_F2_DATA_REG_TO_A_CH <= '0';
+      PS_F2_REG_BUS <= tv;
+      wait for 30 ns;
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),'0',testName,"3A BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),'1',testName,"3B BIT " & INTEGER'Image(bbit));
+            check1(LAMPS_A_CH(bbit),'0',testName,"3C BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),'0',testName,"3D BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),'1',testName,"3E BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+
+      PS_GATE_F2_DATA_REG_TO_A_CH <= '1';
+      wait for 30 ns;      
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),tv(bbit),testName,"3F BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"3G BIT " & INTEGER'Image(bbit));
+            check1(LAMPS_A_CH(bbit),tv(bbit),testName,"3H BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),tv(bbit),testName,"3I BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"3J BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+   end loop;
+
+   PS_GATE_F2_DATA_REG_TO_A_CH <= '0';
+   PS_E2_REG_BUS <= "10101010";
+   PS_F2_REG_BUS <= "01010101";
+   
+   for t1 in 0 to 255 loop
+      tv := std_logic_vector(to_unsigned(t1,tv'Length));
+      PS_GATE_OP_MOD_REG_TO_A_CH <= '0';
+      PS_OP_MOD_REG_BUS <= tv;
+      wait for 30 ns;
+      for bbit in 0 to 7 loop            
+            check1(PS_A_CH_BUS(bbit),'0',testName,"3A BIT " & INTEGER'Image(bbit));
+            check1(PS_A_CH_NOT_BUS(bbit),'1',testName,"3B BIT " & INTEGER'Image(bbit));
+            check1(LAMPS_A_CH(bbit),'0',testName,"3C BIT " & INTEGER'Image(bbit));
+            if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+               check1(PB_A_CH_BUS(bbit),'0',testName,"3D BIT " & INTEGER'Image(bbit));
+            end if;
+            if(bbit /= 7 and bbit /= 5) then
+               check1(PB_A_CH_NOT_BUS(bbit),'1',testName,"3E BIT " & INTEGER'Image(bbit));
+            end if;
+      end loop;
+
+      PS_GATE_OP_MOD_REG_TO_A_CH <= '1';
+      wait for 30 ns;      
+      for bbit in 0 to 7 loop
+            if(bbit /= 6) then -- No WM bit from Op Mod Reg
+               check1(PS_A_CH_BUS(bbit),tv(bbit),testName,"3F BIT " & INTEGER'Image(bbit));
+               check1(PS_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"3G BIT " & INTEGER'Image(bbit));
+               check1(LAMPS_A_CH(bbit),tv(bbit),testName,"3H BIT " & INTEGER'Image(bbit));
+               if(bbit /= 7) then -- No +B check bit, and no +B NOT check or B bits
+                  check1(PB_A_CH_BUS(bbit),tv(bbit),testName,"3I BIT " & INTEGER'Image(bbit));
+               end if;
+               if(bbit /= 7 and bbit /= 5) then
+                  check1(PB_A_CH_NOT_BUS(bbit),not tv(bbit),testName,"3J BIT " & INTEGER'Image(bbit));
+               end if;
+            end if;                        
+      end loop;
+   end loop;
+
 
    assert false report "Simulation Ended NORMALLY" severity failure;
 
