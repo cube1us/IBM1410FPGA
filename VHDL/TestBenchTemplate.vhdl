9258dfc1a50fa7419b1fbdeb47e7e45b24be66b7
Jay Jaeger <cube1us@gmail.com>
2020-09-13 20:23:52 +0000

    Add test vector loop to test bench template
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2276 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/TestBenchTemplate.vhdl
+++ VHDL/TestBenchTemplate.vhdl
@@ -15,13 +15,50 @@ uut_process: process
 
    variable testName: string(1 to 18);
    variable subtest: integer;
-   variable tv: std_logic_vector(15 downto 0);
-   variable a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p: std_logic;
-   variable g1, g2, g3, g4, g5, g6: std_logic;
+   variable tv: std_logic_vector(25 downto 0);
+   variable a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
+   variable g1, g2, g3, g4, g5, g6, g7, g8, g9, g10: std_logic;
 
    begin
 
    -- Your test bench code
+
+   testName := "15.49.04.1        ";
+
+   for tt in 0 to 2**23 loop
+      tv := std_logic_vector(to_unsigned(tt,tv'Length));
+      a := tv(0);
+      b := tv(1);
+      c := tv(2);
+      d := tv(3);
+      e := tv(4);
+      f := tv(5);
+      g := tv(6);
+      h := tv(7);
+      i := tv(8);
+      j := tv(9);
+      k := tv(10);
+      l := tv(11);
+      m := tv(12);
+      n := tv(13);
+      o := tv(14);
+      p := tv(15);
+      q := tv(16);
+      r := tv(17);
+      s := tv(18);
+      t := tv(19);
+      u := tv(20);
+      v := tv(21);
+      w := tv(22);
+      x := tv(23);
+      y := tv(24);
+      z := tv(25);
+
+      
+      wait for 30 ns;
+      
+      
+   end loop;
 
    assert false report "Simulation Ended NORMALLY" severity failure;
 
