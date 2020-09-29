b004c267ba76998746615970724465aefc8b8405
Jay Jaeger <cube1us@gmail.com>
2020-09-29 19:13:39 +0000

    Pages 16.14.01.1, 02.1 Group AddOutputTranslatorShiftB Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2360 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/AddOutputTranslatorShiftB_tb.vhdl
+++ VHDL/AddOutputTranslatorShiftB_tb.vhdl
@@ -177,46 +177,57 @@ uut_process: process
    variable tv: std_logic_vector(25 downto 0);
    variable a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
    variable g1, g2, g3, g4, g5, g6, g7, g8, g9, g10: std_logic;
+   variable carry, ab0, bb0, ab1, bb1: std_logic;
 
    begin
 
    -- Your test bench code
 
-   testName := "15.49.04.1        ";
+   testName := "16.14.01.1 - 02.1 ";
 
-   for tt in 0 to 2**23 loop
+   for tt in 0 to 2**5 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
-      a := tv(0);
-      b := tv(1);
-      c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      i := tv(8);
-      j := tv(9);
-      k := tv(10);
-      l := tv(11);
-      m := tv(12);
-      n := tv(13);
-      o := tv(14);
-      p := tv(15);
-      q := tv(16);
-      r := tv(17);
-      s := tv(18);
-      t := tv(19);
-      u := tv(20);
-      v := tv(21);
-      w := tv(22);
-      x := tv(23);
-      y := tv(24);
-      z := tv(25);
+      carry := tv(0);
+      ab0 := tv(1);
+      ab1 := tv(2);
+      bb0 := tv(3);
+      bb1 := tv(4);
 
+   	PB_AB0_DOT_BB0_DOT_NC <= not carry and ab0 and bb0;
+   	PB_AB0_DOT_BB1_DOT_C <= carry and ab0 and bb1;
+   	PB_AB1_DOT_BB0_DOT_C <= carry and ab1 and bb0;
+   	PB_AB1_DOT_BB1_DOT_NC <= not carry and ab1 and bb1;
+   	PB_AB0_DOT_BB1_DOT_NC <= not carry and ab0 and bb1;
+   	PB_AB1_DOT_BB0_DOT_NC <= not carry and ab1 and bb0;
+   	PB_AB0_DOT_BB0_DOT_C <= carry and ab0 and bb0;
+   	PB_AB1_DOT_BB1_DOT_C <= carry and ab1 and bb1;
+
+      a := ab0 and bb0 and not carry;
+      b := ab1 and bb1 and carry;
+      c := ab0 and bb0 and carry;
+      d := ab0 and bb1 and not carry;
+      e := ab1 and bb0 and not carry;
+      f := ab0 and bb1 and carry;
+      g := ab1 and bb1 and not carry;
+      h := ab1 and bb0 and carry;
       
       wait for 30 ns;
       
+      check1(MB_B0_SHIFT,not a,testName,"B0 Shift");
+      check1(PS_B3_SHIFT,b,testName,"+S B3 Shift");
+      check1(MB_B3_SHIFT,not b,testName,"-B B3 Shift");
+      check1(PS_B1_SHIFT,d or e or c,testName,"+S B1 Shift");
+      check1(MB_B1_SHIFT,not(d or e or c),testName,"-B B1 Shift");
+      check1(PS_B2_SHIFT,f or h or g,testName,"+S B2 Shift");
+      check1(MB_B2_SHIFT,not(f or h or g),testName,"-B B2 Shift");
+      check1(PB_ADDER_OUT_NOT_1_BIT,a or f or h or g,testName,"+B Not 1 Bit");
+      check1(MB_ADDER_OUT_NOT_1_BIT,NOT(PB_ADDER_OUT_NOT_1_BIT),testName,"-B Not 1 Bit");
+      check1(MB_B2_OR_B3_SHIFT,not(f or g or h or b),testName,"-B B2+B3 Shift");
+      check1(PB_ADDER_OUT_1_BIT,b or c or d or e,testName,"+B Adder Out 1 Bit");
+      check1(MB_ADDER_OUT_1_BIT,NOT PB_ADDER_OUT_1_BIT,testName,"+B Adder Out 1 Bit");
+      check1(MB_B0_OR_B1_SHIFT,not(a or c or e or d),testName,"-B B0+B1 Shift");
       
+                  
    end loop;
 
    assert false report "Simulation Ended NORMALLY" severity failure;
