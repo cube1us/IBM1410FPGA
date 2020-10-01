7d05505511b549150fc7138101e5e1866c1e2d73
Jay Jaeger <cube1us@gmail.com>
2020-10-01 15:10:14 +0000

    Page 16.14.11.1, 12.1 Group ZeroBalanceLatches: Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2369 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ZeroBalanceLatches_tb.vhdl
+++ VHDL/ZeroBalanceLatches_tb.vhdl
@@ -156,6 +156,8 @@ fpga_clk_process: process
 -- End of TestBenchFPGAClock.vhdl
 --   
 
+MS_ASSEMBLY_CH_BUS <= NOT PS_ASSEMBLY_CH_BUS;
+
 -- Place your test bench code in the uut_process
 
 uut_process: process
@@ -170,41 +172,133 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        ";
+   testName := "16.14.11.1, 12.1  ";
 
-   for tt in 0 to 2**23 loop
+   for tt in 0 to 2**14 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
       a := tv(0);
       b := tv(1);
       c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      i := tv(8);
-      j := tv(9);
-      k := tv(10);
-      l := tv(11);
-      m := tv(12);
-      n := tv(13);
-      o := tv(14);
-      p := tv(15);
-      q := tv(16);
-      r := tv(17);
-      s := tv(18);
-      t := tv(19);
-      u := tv(20);
-      v := tv(21);
-      w := tv(22);
-      x := tv(23);
-      y := tv(24);
-      z := tv(25);
+      -- d := tv(3);
+      e := tv(3);
+      f := tv(4);
+      g := tv(5);
+      h := tv(6);
+      i := tv(7);
+      j := tv(8);
+      k := tv(9);
+      l := tv(10);
+      m := tv(11);
+      n := tv(12);
+      o := tv(13);
+      
+      g1 := e and (g or f or h) and (not j or k or m or not l);
+      g2 := c or (a and b and e);
+      g3 := c or (o and b and n);
 
-      
+      MS_COMPUTER_RESET_1 <= '0';
       wait for 30 ns;
+      MS_COMPUTER_RESET_1 <= '1';
+      wait for 30 ns;
+    
+      check1(PS_ZR_BAL_LATCH,'0',testName,"C Reset +S Zero Balance");
+      check1(MS_ZR_BAL_LATCH,'1',testName,"C Reset -S Zero Balance");
+      check1(LAMP_15A1K12,'0',testName,"C Reset Zero Balance Lamp");
+      check1(PS_NOT_ZR_BAL_LATCH,'1',testName,"C Reset +S Not Zero Balance");
+      check1(MS_NOT_ZR_BAL_LATCH,'0',testName,"C Reset -S Not Zero Balance");
       
+      -- First, the set ZB / Reset Not ZB tests
       
+ 	   PS_LAST_LOGIC_GATE_2 <= e;
+      PS_LAST_INSN_RO_CYCLE_2 <= a;
+      PS_ADD_TYPE_OP_CODES <= b;
+      MS_MPLY_DOT_LAST_INSN_RO_CYCLE <= not c;
+      MS_A_OR_S_DOT_B_CYCLE <= not f;
+      MS_MPLY_DOT_U_OR_Y_OR_X_DOT_B <= not g;
+      MS_B_CH_INSERT_PLUS_ZERO <= not h;
+      PS_LOGIC_GATE_D_1 <= n;
+      PS_UNITS_LATCH <= o;
+      PS_ASSEMBLY_CH_BUS <= "000" & j & k & l & m;
+
+      wait for 30 ns;
+
+      -- If we are trying to both set and reset ZB, skip these tests
+      
+      if(not(g1 = '1' and g3 = '1')) then
+         check1(PS_ZR_BAL_LATCH,g3,testName,"Set +S Zero Balance");
+         check1(MS_ZR_BAL_LATCH,NOT PS_ZR_BAL_LATCH,testName,"Set -S Zero Balance");
+         check1(LAMP_15A1K12,PS_ZR_BAL_LATCH,testName,"Set Zero Balance Lamp");
+      end if;
+
+      -- Same deal with the NOT ZB Latch
+      
+      if(not(g1 = '1' and g2 = '1')) then
+         check1(PS_NOT_ZR_BAL_LATCH,NOT(g2),testName,"Reset +S Not Zero Balance");
+         check1(MS_NOT_ZR_BAL_LATCH,NOT PS_NOT_ZR_BAL_LATCH,testName,"Reset -S Not Zero Balance");
+      end if;
+      
+      -- Set everything back before the flip side of the test
+      
+     PS_LAST_LOGIC_GATE_2 <= '0';
+     PS_LAST_INSN_RO_CYCLE_2 <= '0';
+     PS_ADD_TYPE_OP_CODES <= '0';
+     MS_A_OR_S_DOT_B_CYCLE <= '1';
+     MS_MPLY_DOT_U_OR_Y_OR_X_DOT_B <= '1';
+     MS_B_CH_INSERT_PLUS_ZERO <= '1';
+     PS_LOGIC_GATE_D_1 <= '0';
+     PS_UNITS_LATCH <= '0';
+     PS_ASSEMBLY_CH_BUS <= "0000000";
+     
+     -- Now, force ZB SET and Not ZB RESET (in other words, set g2 and g3)
+
+     MS_MPLY_DOT_LAST_INSN_RO_CYCLE <= '0';
+     wait for 30 ns;
+     
+     check1(PS_ZR_BAL_LATCH,'1',testName,"Force SET +S Zero Balance");
+     check1(MS_ZR_BAL_LATCH,'0',testName,"Force SET -S Zero Balance");
+     check1(LAMP_15A1K12,'1',testName,"Force SET Zero Balance Lamp");
+     check1(PS_NOT_ZR_BAL_LATCH,'0',testName,"Force Reset +S Not Zero Balance");
+     check1(MS_NOT_ZR_BAL_LATCH,'1',testName,"Force Reset -S Not Zero Balance");
+     
+     -- Set the signals back to the test vector again
+     
+ 	  PS_LAST_LOGIC_GATE_2 <= e;
+     PS_LAST_INSN_RO_CYCLE_2 <= a;
+     PS_ADD_TYPE_OP_CODES <= b;
+     MS_MPLY_DOT_LAST_INSN_RO_CYCLE <= not c;
+     MS_A_OR_S_DOT_B_CYCLE <= not f;
+     MS_MPLY_DOT_U_OR_Y_OR_X_DOT_B <= not g;
+     MS_B_CH_INSERT_PLUS_ZERO <= not h;
+     PS_LOGIC_GATE_D_1 <= n;
+     PS_UNITS_LATCH <= o;
+     PS_ASSEMBLY_CH_BUS <= "000" & j & k & l & m;
+     
+     wait for 30 ns;
+     
+      if(not(g1 = '1' and g3 = '1')) then
+        check1(PS_ZR_BAL_LATCH,not g1,testName,"Reset +S Zero Balance");
+        check1(MS_ZR_BAL_LATCH,NOT PS_ZR_BAL_LATCH,testName,"Reset -S Zero Balance");
+        check1(LAMP_15A1K12,PS_ZR_BAL_LATCH,testName,"Reset Zero Balance Lamp");
+     end if;
+     
+      if(not(g1 = '1' and g2 = '1')) then
+        check1(PS_NOT_ZR_BAL_LATCH,g1,testName,"SET +S Not Zero Balance");
+        check1(MS_NOT_ZR_BAL_LATCH,NOT PS_NOT_ZR_BAL_LATCH,testName,"SET -S Not Zero Balance");
+     end if;
+          
+     -- Set everything back before the reset leading into the next test
+     
+      PS_LAST_LOGIC_GATE_2 <= '0';
+      PS_LAST_INSN_RO_CYCLE_2 <= '0';
+      PS_ADD_TYPE_OP_CODES <= '0';
+      MS_MPLY_DOT_LAST_INSN_RO_CYCLE <= '1';
+      MS_A_OR_S_DOT_B_CYCLE <= '1';
+      MS_MPLY_DOT_U_OR_Y_OR_X_DOT_B <= '1';
+      MS_B_CH_INSERT_PLUS_ZERO <= '1';
+      PS_LOGIC_GATE_D_1 <= '0';
+      PS_UNITS_LATCH <= '0';
+      PS_ASSEMBLY_CH_BUS <= "0000000";
+            
    end loop;
 
    assert false report "Simulation Ended NORMALLY" severity failure;
@@ -218,7 +312,7 @@ uut_process: process
 
 stop_simulation: process
    begin
-   wait for 2 ms;  -- Determines how long your simulation runs
+   wait for 80 ms;  -- Determines how long your simulation runs
    assert false report "Simulation Ended NORMALLY (TIMEOUT)" severity failure;
    end process;
 
