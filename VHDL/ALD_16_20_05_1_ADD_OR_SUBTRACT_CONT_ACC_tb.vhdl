246dca9d38ea4c5421f289223b14983fbc122c71
Jay Jaeger <cube1us@gmail.com>
2020-10-02 19:07:15 +0000

    Page 16.20.05.1 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2387 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_16_20_05_1_ADD_OR_SUBTRACT_CONT_ACC_tb.vhdl
+++ VHDL/ALD_16_20_05_1_ADD_OR_SUBTRACT_CONT_ACC_tb.vhdl
@@ -181,9 +181,9 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        ";
+   testName := "16.20.05.1        ";
 
-   for tt in 0 to 2**23 loop
+   for tt in 0 to 2**14 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
       a := tv(0);
       b := tv(1);
@@ -193,29 +193,44 @@ uut_process: process
       f := tv(5);
       g := tv(6);
       h := tv(7);
-      i := tv(8);
-      j := tv(9);
-      k := tv(10);
-      l := tv(11);
-      m := tv(12);
-      n := tv(13);
-      o := tv(14);
-      p := tv(15);
-      q := tv(16);
-      r := tv(17);
-      s := tv(18);
-      t := tv(19);
-      u := tv(20);
-      v := tv(21);
-      w := tv(22);
-      x := tv(23);
-      y := tv(24);
-      z := tv(25);
+      j := tv(8);
+      k := tv(9);
+      l := tv(10);
+      m := tv(11);
+      n := tv(12);
+      o := tv(13);
 
+      PS_1ST_SCAN <= a;
+		PS_COMPLEMENT_LATCH <= b;
+		PB_ADDER_NO_CARRY <= c;
+		PB_ADDER_CARRY <= d;
+		PS_A_OR_S_DOT_B_CYCLE <= e;
+		PS_UNITS_LATCH <= f;
+		PS_1401_MODE_1 <= g;
+		PB_B_CH_WM_BIT <= h;
+		PS_EXTENSION_LATCH <= j;
+		PS_BODY_OR_EXTENSION <= k;
+		MS_1401_MODE_1 <= not l;
+		PB_B_CH_NOT_WM_BIT <= m;
+		PB_COMPLEMENT_LATCH <= n;
+		PB_1401_MODE <= o;
       
       wait for 30 ns;
-      
-      
+
+      check1(MB_A_OR_S_DOT_B_DOT_1_DOT_S_DOT_BW_DOT_NOT_RC,not(e and a and b and h and c),
+         testName,"a+s.b.1.s.bw.not rc");
+      check1(MB_A_OR_S_DOT_B_DOT_1_DOT_S_DOT_BW_DOT_RC,not(e and a and b and h and d),
+         testName,"a+s.b.1.s.bw.rc");
+      check1(MS_A_OR_S_DOT_B_DOT_1_DOT_S_DOT_U_DOT_1401,not (e and a and b and f and g),
+         testName,"a+s.b.1.s.u.1401");
+      check1(MS_A_OR_S_DOT_B_DOT_1_DOT_S_DOT_X,not(e and a and b and j),testName,
+         "a+s.b.i.s.x");
+      check1(MS_A_OR_S_DOT_B_DOT_Y_OR_X,not(e and k),testName,"a+s.b.y+x");
+      check1(MS_A_OR_S_DOT_B_DOT_Y_OR_X_DOT_NOT_1401,not(e and k and not l),testName,
+         "a+s.b.y+x.not 1401");
+      check1(MB_A_OR_S_DOT_B_DOT_Y_OR_X_DOT_S_OR_NOT_BW_DOT_1401,not(e and k and (n or m) and o),
+         testName,"a+s.b.y+x.s.not bw.1401");
+            
    end loop;
 
    assert false report "Simulation Ended NORMALLY" severity failure;
