992d03acac10ba725c5354b319a82d738fc74a10
Jay Jaeger <cube1us@gmail.com>
2020-10-03 19:24:54 +0000

    Page 16.20.14.1 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2400 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_16_20_14_1_TRUE_AND_COMP_LATCHES_ACC_tb.vhdl
+++ VHDL/ALD_16_20_14_1_TRUE_AND_COMP_LATCHES_ACC_tb.vhdl
@@ -164,43 +164,148 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        ";
+   testName := "16.20.14.1        ";
 
-   for tt in 0 to 2**23 loop
+   -- Test reset
+   
+   MS_PROGRAM_RESET_5 <= '0';
+   wait for 30 ns;
+   MS_PROGRAM_RESET_5 <= '1';
+   wait for 30 ns;
+
+   check1(PS_TRUE_LATCH,'0',testName,"Reset True Latch");
+   check1(PS_TRUE_LATCH_1,'0',testName,"Reset True Latch 1");
+   check1(PB_TRUE_ADD_A,'0',testName,"Reset True Add A");
+   
+   -- Test True add A except for True Latch Input
+   
+   for tt in 0 to 2**3 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
-      a := tv(0);
-      b := tv(1);
-      c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      i := tv(8);
-      j := tv(9);
-      k := tv(10);
-      l := tv(11);
-      m := tv(12);
-      n := tv(13);
-      o := tv(14);
-      p := tv(15);
-      q := tv(16);
-      r := tv(17);
-      s := tv(18);
-      t := tv(19);
-      u := tv(20);
-      v := tv(21);
-      w := tv(22);
-      x := tv(23);
-      y := tv(24);
-      z := tv(25);
-
-      
+      PS_ADDER_A_CH_USE_T_OR_C <= tv(0);
+      MS_X_CYCLE <= not tv(1);
+      MS_LB_OP_DOT_1ST_OR_3RD_SCAN <= not tv(2);
       wait for 30 ns;
-      
-      
+      check1(PB_TRUE_ADD_A,tv(1) or tv(2),testName,"True Add A no True Latch");
    end loop;
+   
+   PS_ADDER_A_CH_USE_T_OR_C <= '0'; -- For next set of tests
+   MS_X_CYCLE <= '1';
+   MS_LB_OP_DOT_1ST_OR_3RD_SCAN <= '1';
+   
+   -- Test True Ctrl and rest of True Add A   
+   
+   for tt in 0 to 2**4 loop
+      tv := std_logic_vector(to_unsigned(tt,tv'Length));
+      MS_PROGRAM_RESET_5 <= '0';
+      wait for 30 ns;
+      MS_PROGRAM_RESET_5 <= '1';
+      wait for 30 ns;
+      check1(PS_TRUE_LATCH,'0',testName,"1A");
+      check1(PS_TRUE_LATCH_1,'0',testName,"1B");
+      check1(PB_TRUE_ADD_A,'0',testName,"1C");
+      PS_SET_TRUE <= tv(0);
+      PS_LAST_LOGIC_GATE_2 <= tv(1);
+      PS_LOGIC_GATE_C_1 <= tv(2);
+      PS_ADDER_A_CH_USE_T_OR_C <= tv(3);
+      wait for 60 ns; -- perhaps set latches (cascaced)      
+      PS_SET_TRUE <= '0';
+      PS_LAST_LOGIC_GATE_2 <= '1';
+      check1(PS_TRUE_LATCH,tv(0) and tv(1) and tv(2),testName,"1D");
+      check1(PS_TRUE_LATCH_1,tv(0) and tv(1) and tv(2),testName,"1E");
+      check1(PB_TRUE_ADD_A,tv(0) and tv(1) and tv(2) and tv(3),testName,"1F");
+      -- Reset True Ctrl and then True Latch
+      MS_LOGIC_GATE_D_1 <= '0';
+      wait for 30 ns;
+      MS_LOGIC_GATE_D_1 <= '1';
+      wait for 60 ns; -- Resetting True Ctrl won't reset True Latch
+      check1(PS_TRUE_LATCH,tv(0) and tv(1) and tv(2),testName,"1G");
+      check1(PS_TRUE_LATCH_1,tv(0) and tv(1) and tv(2),testName,"1H");
+      check1(PB_TRUE_ADD_A,tv(0) and tv(1) and tv(2) and tv(3),testName,"1I");
+      -- But then resetting True Latch should work
+      MS_LOGIC_GATE_B_1 <= '0';
+      wait for 30 ns;
+      MS_LOGIC_GATE_B_1 <= '1';
+      wait for 30 ns;
+      check1(PS_TRUE_LATCH,'0',testName,"1J");
+      check1(PS_TRUE_LATCH_1,'0',testName,"1K");
+      check1(PB_TRUE_ADD_A,'0',testName,"1L");            
+   end loop;
+   
+   -- Test True Latch w/o True Ctrl
+   
+   PS_SET_TRUE <= '0';
+   PS_LAST_LOGIC_GATE_2 <= '0';
+   PS_LOGIC_GATE_C_1 <= '0';
+   PS_ADDER_A_CH_USE_T_OR_C <= '0';
 
+   for tt in 0 to 3 loop
+      tv := std_logic_vector(to_unsigned(tt,tv'Length));
+      MS_PROGRAM_RESET_5 <= '0';
+      wait for 30 ns;
+      MS_PROGRAM_RESET_5 <= '1';
+      wait for 30 ns;
+      MB_START_TRUE_ADD_1 <= not tv(0);
+      MB_START_TRUE_ADD_2 <= not tv(1);
+      wait for 30 ns;
+      check1(PS_TRUE_LATCH,tv(0) or tv(1),testName,"2A");
+      check1(PS_TRUE_LATCH_1,tv(0) or tv(1),testName,"2B");      
+   end loop;
+   
+   -- Test Regen
+   
+   MB_START_TRUE_ADD_1 <= '1';
+   MB_START_TRUE_ADD_2 <= '1';
+   MS_PROGRAM_RESET_5 <= '0';
+   wait for 30 ns;
+   MS_PROGRAM_RESET_5 <= '1';
+   wait for 30 ns;
+   
+   MB_START_TRUE_ADD_1 <= '0';
+   wait for 30 ns; -- Sets True Latch
+   MB_START_TRUE_ADD_1 <= '1';
+   wait for 30 ns; -- Should stay set
+   check1(PS_TRUE_LATCH,'1',testName,"3A");
+   check1(PS_TRUE_LATCH_1,'1',testName,"3B");
+   -- Set True Ctrl latch from True Latch regen      
+   PS_REGEN_TRUE <= '1'; 
+   PS_LAST_LOGIC_GATE_2 <= '1';
+   wait for 30 ns;
+   -- Reset True Latch (and reset inputs to True Ctrl Latch - which should stay set)
+   MS_LOGIC_GATE_B_1 <= '0';
+   PS_REGEN_TRUE <= '0'; 
+   PS_LAST_LOGIC_GATE_2 <= '0';
+   wait for 30 ns;
+   MS_LOGIC_GATE_B_1 <= '1';
+   wait for 30 ns;
+   check1(PS_TRUE_LATCH,'0',testName,"3C");
+   check1(PS_TRUE_LATCH_1,'0',testName,"3D");
+   -- Now set True latch via True Ctrl
+   PS_LOGIC_GATE_C_1 <= '1';
+   wait for 30 ns; -- Should set True Latch
+   PS_LOGIC_GATE_C_1 <= '0';
+   wait for 30 ns;
+   -- Reset True Ctrl
+   MS_LOGIC_GATE_D_1 <= '0';
+   wait for 30 ns;
+   MS_LOGIC_GATE_D_1 <= '1';
+   wait for 30 ns;
+   check1(PS_TRUE_LATCH,'1',testName,"3E");
+   check1(PS_TRUE_LATCH_1,'1',testName,"3F");
+   -- Reset True Latch (so now, both are reset)
+   MS_LOGIC_GATE_B_1 <= '0';
+   wait for 30 ns;
+   MS_LOGIC_GATE_B_1 <= '1';
+   wait for 30 ns;
+   check1(PS_TRUE_LATCH,'0',testName,"3G");
+   check1(PS_TRUE_LATCH_1,'0',testName,"3H");
+   -- Since True Ctrl is NOT set any more, this should NOT set True Latch either
+   PS_LOGIC_GATE_C_1 <= '1';
+   wait for 30 ns;
+   PS_LOGIC_GATE_C_1 <= '0';
+   wait for 30 ns;
+   check1(PS_TRUE_LATCH,'0',testName,"3I");
+   check1(PS_TRUE_LATCH_1,'0',testName,"3J");
+          
    assert false report "Simulation Ended NORMALLY" severity failure;
 
    wait;
