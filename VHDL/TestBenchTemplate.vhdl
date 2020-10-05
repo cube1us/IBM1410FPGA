598198f9769c717b376c2f88989aa91167ac4b73
Jay Jaeger <cube1us@gmail.com>
2020-10-05 19:00:06 +0000

    TestBenchTemplate.vhdl: Remove variable "i" which is rarely used.
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2437 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/TestBenchTemplate.vhdl
+++ VHDL/TestBenchTemplate.vhdl
@@ -16,16 +16,16 @@ uut_process: process
    variable testName: string(1 to 18);
    variable subtest: integer;
    variable tv: std_logic_vector(25 downto 0);
-   variable a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
+   variable a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
    variable g1, g2, g3, g4, g5, g6, g7, g8, g9, g10: std_logic;
 
    begin
 
    -- Your test bench code
 
-   testName := "15.49.04.1        ";
+   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
 
-   for tt in 0 to 2**23 loop
+   for tt in 0 to 2**25 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
       a := tv(0);
       b := tv(1);
@@ -35,24 +35,23 @@ uut_process: process
       f := tv(5);
       g := tv(6);
       h := tv(7);
-      i := tv(8);
-      j := tv(9);
-      k := tv(10);
-      l := tv(11);
-      m := tv(12);
-      n := tv(13);
-      o := tv(14);
-      p := tv(15);
-      q := tv(16);
-      r := tv(17);
-      s := tv(18);
-      t := tv(19);
-      u := tv(20);
-      v := tv(21);
-      w := tv(22);
-      x := tv(23);
-      y := tv(24);
-      z := tv(25);
+      j := tv(8);
+      k := tv(9);
+      l := tv(10);
+      m := tv(11);
+      n := tv(12);
+      o := tv(13);
+      p := tv(14);
+      q := tv(15);
+      r := tv(16);
+      s := tv(17);
+      t := tv(18);
+      u := tv(19);
+      v := tv(20);
+      w := tv(21);
+      x := tv(22);
+      y := tv(23);
+      z := tv(24);
 
       
       wait for 30 ns;
