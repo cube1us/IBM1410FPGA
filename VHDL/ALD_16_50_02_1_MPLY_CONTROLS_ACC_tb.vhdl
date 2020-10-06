c2b0dfc394cc2b62c67b0638953c87d638734600
Jay Jaeger <cube1us@gmail.com>
2020-10-06 14:16:12 +0000

    Page 16.50.02.1 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2450 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_16_50_02_1_MPLY_CONTROLS_ACC_tb.vhdl
+++ VHDL/ALD_16_50_02_1_MPLY_CONTROLS_ACC_tb.vhdl
@@ -155,9 +155,9 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
+   testName := "16.50.02.1        ";  -- NOTE:  Remove X when editing to set correct length!
 
-   for tt in 0 to 2**25 loop
+   for tt in 0 to 2**7 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
       a := tv(0);
       b := tv(1);
@@ -166,28 +166,24 @@ uut_process: process
       e := tv(4);
       f := tv(5);
       g := tv(6);
-      h := tv(7);
-      j := tv(8);
-      k := tv(9);
-      l := tv(10);
-      m := tv(11);
-      n := tv(12);
-      o := tv(13);
-      p := tv(14);
-      q := tv(15);
-      r := tv(16);
-      s := tv(17);
-      t := tv(18);
-      u := tv(19);
-      v := tv(20);
-      w := tv(21);
-      x := tv(22);
-      y := tv(23);
-      z := tv(24);
 
+		PS_B_CYCLE <= a;
+		PS_EXTENSION_LATCH <= b;
+		PS_MPLY_OP_CODE <= c;
+		PS_NOT_MPLY_DIV_LAST_LATCH <= d;
+		PS_MPLY_DIV_LAST_LATCH <= e;
+		PS_TRUE_LATCH <= f;
+		PS_COMPLEMENT_LATCH <= g;
+      
+      g1 := a and b and c;
       
       wait for 30 ns;
       
+		check1(MS_MPLY_DOT_X_DOT_B_DOT_REGEN_1,not(g1),testName,"1A");
+      check1(MS_MPLY_DOT_X_DOT_B_DOT_NOT_MDL,not(g1 and d),testName,"1A");
+      check1(MS_MPLY_DOT_X_DOT_B_DOT_MDL,not(g1 and e),testName,"1A");
+      check1(MS_MPLY_DOT_X_DOT_B_DOT_T,not(g1 and f),testName,"1A");
+      check1(MS_MPLY_DOT_X_DOT_B_DOT_S,not(g1 and g),testName,"1A");
       
    end loop;
 
