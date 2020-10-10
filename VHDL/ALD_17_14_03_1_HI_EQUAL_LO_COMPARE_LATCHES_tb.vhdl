1f81ba95ea9ae9f368dd95e5d418c5f60ce38bd7
Jay Jaeger <cube1us@gmail.com>
2020-10-10 18:01:21 +0000

    Page 17.14.03.1 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2504 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_17_14_03_1_HI_EQUAL_LO_COMPARE_LATCHES_tb.vhdl
+++ VHDL/ALD_17_14_03_1_HI_EQUAL_LO_COMPARE_LATCHES_tb.vhdl
@@ -170,39 +170,118 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
+   testName := "17.14.03.1        ";
 
-   for tt in 0 to 2**25 loop
+   for tt in 0 to 2**11 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
-      a := tv(0);
-      b := tv(1);
-      c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      j := tv(8);
-      k := tv(9);
-      l := tv(10);
-      m := tv(11);
-      n := tv(12);
-      o := tv(13);
-      p := tv(14);
-      q := tv(15);
-      r := tv(16);
-      s := tv(17);
-      t := tv(18);
-      u := tv(19);
-      v := tv(20);
-      w := tv(21);
-      x := tv(22);
-      y := tv(23);
-      z := tv(24);
-
+      b := tv(0);
+      c := tv(1);
+      d := tv(2);
+      e := tv(3);
+      f := tv(4);
+      g := tv(5);
+      h := tv(6);
+      l := tv(7);
+      m := tv(8);
+      n := tv(9);
+      o := tv(10);
       
+      g1 := h and g and d and not m and l and not b;
+      g2 := n and m and o;
+      g3 := g1 or g2;
+      g4 := b and d;
+      g5 := not e and g and d and f;
+      g6 := g4 or c or g5;
+
+      -- Initial reset
+      
+      MS_COMPUTER_RESET_1 <= '0';
+      wait for 30 ns;
+      MS_COMPUTER_RESET_1 <= '1';
       wait for 30 ns;
       
+      check1(PS_EQUAL,'0',testName,"+S Equal Loop Reset");
+      check1(MS_EQUAL,'1',testName,"-S Equal Loop Reset");      
+      check1(LAMP_15A1C12,'0',testName,"Lamp Equal Loop Reset");
+
+      -- Test Latch Set (not setting signals that would hold it reset)
+            
+      PS_LOGIC_GATE_F_1 <= d;
+      MS_CMP_EQUAL <= not e;
+      PS_EQUAL_LOW_LATCHES_SET <= g;
+      PS_CMP_EQUAL <= h;
+      MS_1401_MODE <= not m;
+      MS_SET_HIGH_CY <= not b;
+      PS_UNITS_LATCH <= l;
+      PS_1401_MODE <= m;
+      PS_COMPARE_OP_CODE <= n;
+      PS_I_RING_4_TIME <= o;      
+      wait for 30 ns; -- Perhaps set the latch
+           
+      check1(PS_EQUAL,g3,testName,"+S Equal Set");
+      check1(MS_EQUAL,not g3,testName,"-S Equal Set");      
+      check1(LAMP_15A1C12,g3,testName,"Lamp Equal Set");
+      
+      -- Set some of the variables back to initial state for reset test.
+      -- Latch state should not change.
+
+      MS_CMP_EQUAL <= not e;
+      PS_EQUAL_LOW_LATCHES_SET <= '0';
+      PS_CMP_EQUAL <= '0';
+      MS_SET_HIGH_CY <= not b;
+      PS_UNITS_LATCH <= '0';
+      wait for 30 ns;
+
+      check1(PS_EQUAL,g3,testName,"+S Equal Still Set");
+      check1(MS_EQUAL,not g3,testName,"-S Equal Still Set");      
+      check1(LAMP_15A1C12,g3,testName,"Lamp Equal Still Set");
+
+      -- Force the latch set.
+
+      PS_COMPARE_OP_CODE <= '1';
+      PS_I_RING_4_TIME <= '1';      
+      PS_1401_MODE <= '1';      
+      wait for 30 ns;            
+      PS_COMPARE_OP_CODE <= '0';
+      PS_I_RING_4_TIME <= '0';      
+      PS_1401_MODE <= m;
+      wait for 30 ns;      
+                 
+      check1(PS_EQUAL,'1',testName,"+S Equal Force Set");
+      check1(MS_EQUAL,'0',testName,"-S Equal Force Set");      
+      check1(LAMP_15A1C12,'1',testName,"Lamp Force Still Set");
+      
+      -- Set the rest of the variables
+      PS_SET_HIGH_CY <= b;
+      MS_EQUAL_LOW_LATCHES_RESET <= not c;
+      PS_2ND_CLOCK_PULSE_2 <= f;
+      PS_LOGIC_GATE_F_1 <= d;
+      MS_CMP_EQUAL <= not e;
+      PS_EQUAL_LOW_LATCHES_SET <= g;
+      MS_1401_MODE <= not m;
+      MS_SET_HIGH_CY <= not b;
+      PS_1401_MODE <= m;
+      wait for 30 ns; -- Perhaps reset the latch (g6 is the reset signal)
+      
+      check1(PS_EQUAL,not g6,testName,"+S Equal Latch Reset");
+      check1(MS_EQUAL,g6,testName,"-S Equal Latch Reset");      
+      check1(LAMP_15A1C12,not g6,testName,"Lamp Equal Reset");
+      
+      -- Set the signals back to their initial state before the next iteration
+      
+   	 PS_SET_HIGH_CY <= '0';
+       MS_EQUAL_LOW_LATCHES_RESET <= '1';
+       PS_LOGIC_GATE_F_1 <= '0';
+       MS_CMP_EQUAL <= '1';
+       PS_2ND_CLOCK_PULSE_2 <= '0';
+       PS_EQUAL_LOW_LATCHES_SET <= '0';
+       PS_CMP_EQUAL <= '0';
+       MS_1401_MODE <= '1';
+       MS_SET_HIGH_CY <= '1';
+       PS_UNITS_LATCH <= '0';
+       PS_1401_MODE <= '0';
+       PS_COMPARE_OP_CODE <= '0';
+       PS_I_RING_4_TIME <= '0';
       
    end loop;
 
@@ -217,7 +296,7 @@ uut_process: process
 
 stop_simulation: process
    begin
-   wait for 2 ms;  -- Determines how long your simulation runs
+   wait for 20 ms;  -- Determines how long your simulation runs
    assert false report "Simulation Ended NORMALLY (TIMEOUT)" severity failure;
    end process;
 
