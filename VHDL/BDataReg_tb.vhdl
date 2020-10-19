da33a125d23bf6bca80d30a294843504af80497f
Jay Jaeger <cube1us@gmail.com>
2020-10-19 17:38:14 +0000

    Pages 36.11.01.1 - 04.1 Group BDataReg Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2624 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/BDataReg_tb.vhdl
+++ VHDL/BDataReg_tb.vhdl
@@ -109,6 +109,17 @@ procedure check1(
     assert checked = val report testname & " (" & test & ") failed." severity failure;
     end procedure;
       
+procedure checkChar(
+    checked: in STD_LOGIC_VECTOR(7 downto 0);
+    val: in STD_LOGIC_VECTOR(7 downto 0);
+    testname: in string;
+    test: in string) is
+    begin
+       for thebit in 0 to 7 loop
+         assert checked(thebit) = val(thebit) report
+            testname & " (" & test & ") bit " & Integer'image(thebit) & " failed." severity failure; 
+       end loop;
+    end procedure;
 
 
    -- Your test bench declarations go here
@@ -183,48 +194,158 @@ uut_process: process
 
    variable testName: string(1 to 18);
    variable subtest: integer;
-   variable tv: std_logic_vector(25 downto 0);
+   variable tv, tv0, tv1, tv2, tv3: std_logic_vector(7 downto 0);
    variable a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
    variable g1, g2, g3, g4, g5, g6, g7, g8, g9, g10: std_logic;
+   variable readout, regen: std_logic;
+   variable readdata, inhdata: std_logic_vector(7 downto 0);
 
    begin
 
    -- Your test bench code
 
-   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
+   testName := "36.11.0%.1        ";
 
-   for tt in 0 to 2**25 loop
-      tv := std_logic_vector(to_unsigned(tt,tv'Length));
-      a := tv(0);
-      b := tv(1);
-      c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      j := tv(8);
-      k := tv(9);
-      l := tv(10);
-      m := tv(11);
-      n := tv(12);
-      o := tv(13);
-      p := tv(14);
-      q := tv(15);
-      r := tv(16);
-      s := tv(17);
-      t := tv(18);
-      u := tv(19);
-      v := tv(20);
-      w := tv(21);
-      x := tv(22);
-      y := tv(23);
-      z := tv(24);
-
+   for charbits in 0 to 2**8 loop
+   
+      -- Reset
       
+      PY_B_DATA_REG_RESET_1 <= '1';
+      PY_B_DATA_REG_RESET_2 <= '1';
+      PY_B_DATA_REG_RESET_3 <= '1';
+      PY_B_DATA_REG_RESET_4 <= '1';
+      MY_RO_CHR_0 <= '0';
+      MY_RO_CHR_1 <= '0';
+      MY_RO_CHR_2 <= '0';
+      MY_RO_CHR_3 <= '0';
+      MY_REGEN_CHR_0 <= '0';
+      MY_REGEN_CHR_1 <= '0';
+      MY_REGEN_CHR_2 <= '0';
+      MY_REGEN_CHR_3 <= '0';
+      wait for 30 ns;
+
+      PY_B_DATA_REG_RESET_1 <= '0';
+      PY_B_DATA_REG_RESET_2 <= '0';
+      PY_B_DATA_REG_RESET_3 <= '0';
+      PY_B_DATA_REG_RESET_4 <= '0';
       wait for 30 ns;
       
+      -- Check that the data is reset and we see the appropriate values on the output
       
+      checkChar(PS_B_DATA_REG_STAR_0_STAR_BUS,"00000000",testName,"Loop Reset RO B Char 0");
+      checkChar(PS_B_DATA_REG_STAR_1_STAR_BUS,"00000000",testName,"Loop Reset RO B Char 1");
+      checkChar(PS_B_DATA_REG_STAR_2_STAR_BUS,"00000000",testName,"Loop Reset RO B Char 2");
+      checkChar(PS_B_DATA_REG_STAR_3_STAR_BUS,"00000000",testName,"Loop Reset RO B Char 3");
+      
+      checkChar(MY_INH_CHAR_0_BUS,"00000000",testName,"Loop Reset Regen B Char 0");
+      checkChar(MY_INH_CHAR_1_BUS,"00000000",testName,"Loop Reset Regen B Char 1");
+      checkChar(MY_INH_CHAR_2_BUS,"00000000",testName,"Loop Reset Regen B Char 2");
+      checkChar(MY_INH_CHAR_3_BUS,"00000000",testName,"Loop Reset Regen B Char 3");
+                  
+      -- Shift data into the 4 planes of b registers so they don't match each other
+      
+      tv0 := std_logic_vector(to_unsigned(charbits,tv0'Length));
+      tv1 := tv0(0) & tv0(7 downto 1);
+      tv2 := tv1(0) & tv1(7 downto 1);
+      tv3 := tv2(0) & tv2(7 downto 1);
+                  
+      MY_SA_CHAR_0_BUS <= not tv0;
+      MY_SA_CHAR_1_BUS <= not tv1;
+      MY_SA_CHAR_2_BUS <= not tv2;
+      MY_SA_CHAR_3_BUS <= not tv3;
+      wait for 30 ns; -- Set the registers
+
+      -- Once set, the register latches should stay set
+      
+      MY_SA_CHAR_0_BUS <= "11111111";
+      MY_SA_CHAR_1_BUS <= "11111111";
+      MY_SA_CHAR_2_BUS <= "11111111";
+      MY_SA_CHAR_3_BUS <= "11111111";
+      wait for 30 ns; -- Set the registers
+      
+      -- Now, for each individual character, loop through all of the RO/Regen possibilities
+      -- for each of the 4 bits independently
+      
+      for tt in 0 to 2**8 loop
+         tv := std_logic_vector(to_unsigned(tt,tv'Length));
+         a := tv(0);
+         b := tv(1);
+         c := tv(2);
+         d := tv(3);
+         e := tv(4);
+         f := tv(5);
+         g := tv(6);
+         h := tv(7);
+         
+         -- Set the signals accordingly
+
+   		MY_RO_CHR_0 <= not a;
+         MY_REGEN_CHR_0 <= not b;
+         MY_RO_CHR_1 <= not c;
+         MY_REGEN_CHR_1 <= not d;
+         MY_RO_CHR_2 <= not e;
+         MY_REGEN_CHR_2 <= not f;
+         MY_RO_CHR_3 <= not g;
+         MY_REGEN_CHR_3 <= not h;         
+         wait for 30 ns;
+         
+         -- Figure out what the data should look like, and check it.
+         -- Readout Data and Inhibit data are independent
+         
+         for charPlane in 0 to 3 loop
+            
+            readout := tv(charPlane * 2);
+            regen := tv((charPlane * 2) + 1);
+            
+            if(readout = '1') then
+               case charPlane is
+                  when 0 => readdata :=  tv0;                 
+                  when 1 => readdata :=  tv1;                 
+                  when 2 => readdata :=  tv2;                 
+                  when 3 => readdata :=  tv3;                 
+               end case;
+            else
+               readdata := "00000000";
+            end if;
+            
+            if(regen = '1') then
+               case charPlane is
+                  when 0 => inhdata :=  tv0;                 
+                  when 1 => inhdata :=  tv1;                 
+                  when 2 => inhdata :=  tv2;                 
+                  when 3 => inhdata :=  tv3;                 
+               end case;
+            else
+               inhdata := "00000000";
+            end if;
+
+            case charPlane is
+               when 0 =>
+                  checkChar(PS_B_DATA_REG_STAR_0_STAR_BUS,readdata,testName,
+                     "Readout Char 0, RO is " & std_logic'image(readout));
+                  checkChar(MY_INH_CHAR_0_BUS,inhdata,testName,
+                     "Regen Char 0, Regen is " & std_logic'image(regen));
+               when 1 =>
+                   checkChar(PS_B_DATA_REG_STAR_1_STAR_BUS,readdata,testName,
+                      "Readout Char 1, RO is " & std_logic'image(readout));
+                  checkChar(MY_INH_CHAR_1_BUS,inhdata,testName,
+                         "Regen Char 1, Regen is " & std_logic'image(regen));
+               when 2 =>
+                   checkChar(PS_B_DATA_REG_STAR_2_STAR_BUS,readdata,testName,
+                      "Readout Char 2, RO is " & std_logic'image(readout));
+                  checkChar(MY_INH_CHAR_2_BUS,inhdata,testName,
+                         "Regen Char 2, Regen is " & std_logic'image(regen));
+               when 3 =>
+                   checkChar(PS_B_DATA_REG_STAR_3_STAR_BUS,readdata,testName,
+                      "Readout Char 3, RO is " & std_logic'image(readout));
+                  checkChar(MY_INH_CHAR_3_BUS,inhdata,testName,
+                         "Regen Char 3, Regen is " & std_logic'image(regen));
+            end case;               
+         
+         end loop;
+               
+      end loop;
+
    end loop;
 
    assert false report "Simulation Ended NORMALLY" severity failure;
@@ -238,7 +359,7 @@ uut_process: process
 
 stop_simulation: process
    begin
-   wait for 2 ms;  -- Determines how long your simulation runs
+   wait for 20 ms;  -- Determines how long your simulation runs
    assert false report "Simulation Ended NORMALLY (TIMEOUT)" severity failure;
    end process;
 
