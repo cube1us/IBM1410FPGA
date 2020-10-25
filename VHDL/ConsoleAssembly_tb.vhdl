3e5b4f12e3d348bf409405d3d011dd08bcfdc63c
Jay Jaeger <cube1us@gmail.com>
2020-10-25 21:29:14 +0000

    Pages 42.10.01.1 - 08.1 Group ConsoleAssembly Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2689 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ConsoleAssembly_tb.vhdl
+++ VHDL/ConsoleAssembly_tb.vhdl
@@ -9,6 +9,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;  -- For use in test benches only
 use WORK.ALL;
+use WORK.BCD;
 
 -- End of include from HDLTemplate.vhdl
 
@@ -123,6 +124,17 @@ procedure check1(
     assert checked = val report testname & " (" & test & ") failed." severity failure;
     end procedure;
       
+procedure checkChar(
+    checked: in STD_LOGIC_VECTOR(7 downto 0);
+    val: in STD_LOGIC_VECTOR(7 downto 0);
+    testname: in string;
+    test: in string) is
+    begin
+       for thebit in 0 to 7 loop
+         assert checked(thebit) = val(thebit) report
+            testname & " (" & test & ") bit " & Integer'image(thebit) & " failed." severity failure; 
+       end loop;
+    end procedure;
 
 
    -- Your test bench declarations go here
@@ -207,46 +219,298 @@ uut_process: process
    variable tv: std_logic_vector(25 downto 0);
    variable a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z: std_logic;
    variable g1, g2, g3, g4, g5, g6, g7, g8, g9, g10: std_logic;
+   
+   variable tc, tc2, results: std_logic_vector(7 downto 0);
+   variable testChar: string(1 to 1) := "~";
 
    begin
 
    -- Your test bench code
 
-   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
+   testName := "42.10.0%.1        ";
 
-   for tt in 0 to 2**25 loop
-      tv := std_logic_vector(to_unsigned(tt,tv'Length));
-      a := tv(0);
-      b := tv(1);
-      c := tv(2);
-      d := tv(3);
-      e := tv(4);
-      f := tv(5);
-      g := tv(6);
-      h := tv(7);
-      j := tv(8);
-      k := tv(9);
-      l := tv(10);
-      m := tv(11);
-      n := tv(12);
-      o := tv(13);
-      p := tv(14);
-      q := tv(15);
-      r := tv(16);
-      s := tv(17);
-      t := tv(18);
-      u := tv(19);
-      v := tv(20);
-      w := tv(21);
-      x := tv(22);
-      y := tv(23);
-      z := tv(24);
+   -- First, test the special characters
+   
+   for BCD_CHAR in BCD.BCD loop
+      tc := BCD.bcd_to_slv8_odd_parity(BCD_CHAR);
+      
 
+      case BCD_CHAR is
+         when BCD.BCD_A =>
+            testChar := "A";
+            MS_SPECIAL_CHAR_A_C_E_I <= '0';            
+         when BCD.BCD_C =>
+            testChar := "C";
+            MS_SPECIAL_CHAR_A_C_E_I <= '0';
+            MS_SPECIAL_CHAR_C <= '0';
+         when BCD.BCD_E =>
+            testChar := "E";
+            MS_SPECIAL_CHAR_A_C_E_I <= '0';
+            MS_SPECIAL_CHAR_E <= '0';
+         when BCD.BCD_I =>
+            testChar := "I";
+            MS_SPECIAL_CHAR_A_C_E_I <= '0';
+            MS_SPECIAL_CHAR_I <= '0';
+         when BCD.BCD_B =>
+            testChar := "B";
+            MS_SPECIAL_CHAR_B <= '0';
+         when BCD.BCD_D =>
+            testChar := "D";
+            MS_SPECIAL_CHAR_D <= '0';
+         when BCD.BCD_R =>
+            testChar := "R";
+            MS_SPECIAL_CHAR_R <= '0';
+         when BCD.BCD_S =>
+            testChar := "S";
+            MS_SPECIAL_CHAR_S <= '0';
+         when BCD.BCD_POUND =>
+            testChar := "#";
+            MS_SPECIAL_CHAR_POUND <= '0';                               
+         when others => 
+            testChar := "~";
+            tc := "00000000";  -- No output for these         
+      end case;
+
+      wait for 30ns;
       
-      wait for 30 ns;
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
       
+      checkchar(results,tc,testName,"Console output special character: " & testChar);
+            
+      MS_SPECIAL_CHAR_A_C_E_I <= '1';
+      MS_SPECIAL_CHAR_R <= '1';
+      MS_SPECIAL_CHAR_POUND <= '1';
+      MS_SPECIAL_CHAR_S <= '1';
+      MS_SPECIAL_CHAR_C <= '1';
+      MS_SPECIAL_CHAR_B <= '1';
+      MS_SPECIAL_CHAR_E <= '1';
+      MS_SPECIAL_CHAR_D <= '1';
+      MS_SPECIAL_CHAR_I <= '1';
+      wait for 10 ns;
+            
+   end loop;
+
+   -- Next, test the address register exit channel (that would display IAR, AAR or BAR)
+
+    PS_CONS_ADDR_REG_EXIT_GATE <= '1';         
+   
+   for tt in 0 to 31 loop
+      tc := std_logic_vector(to_unsigned(tt,tc'Length));
+      
+      for tp in 0 to 3 loop
+         tv := std_logic_vector(to_unsigned(tp,tv'Length));
+         tc(HDL_A_BIT) := tv(0) and tv(1);
+         tc(HDL_C_BIT) := tv(2);
+         
+         PS_AR_EXIT_CH_BUS <= tv(2) & "000" & tc(3 downto 0);
+         PS_AR_CH_STAR_TRANSLATOR_STAR_BUS(HDL_4_BIT) <= tc(HDL_4_BIT);
+         PS_AR_CH_VC_GROUP_ONE <= tv(0);
+         PS_AR_CH_VC_GROUP_TWO <= tv(1);
+         
+         wait for 30 ns;
+         
+         results := PS_CONSOLE_OUTPUT_BUS;
+         results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+         
+         checkChar(results,tc,testName,"Console Address Register Out");         
+      end loop;
+             
+   end loop;
+   
+   PS_CONS_ADDR_REG_EXIT_GATE <= '0';
+   PS_AR_CH_VC_GROUP_ONE <= '0';
+   PS_AR_CH_VC_GROUP_TWO <= '0';
+   
+   -- Then the rest of the registers that are mostly similar
+   
+   for tt in 0 to 255 loop
+      tc := std_logic_vector(to_unsigned(tt,tc'Length));
+
+      -- A Data register
+
+      PS_CONS_MX_POS_BUS(22) <= '1'; -- identifies character position in stop printout
+      PS_A_DATA_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;      
+      checkChar(results,tc,testName,"Console A Register Out");
+      
+      PS_CONS_MX_POS_BUS(22) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console A Register Out Not Enabled");
+      PS_A_DATA_REG_BUS <= "10101010";
+
+      -- B Data register
+
+      PS_CONS_B_DATA_CH_GATE <= '1'; -- Occurs at MX 23 and in other ways
+      PS_B_CH_BUS <= tc;
+      PS_B_CH_WM_BIT_1 <= tc(HDL_WM_BIT);
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;      
+      checkChar(results,tc,testName,"Console B Register Out");
+      
+      PS_CONS_B_DATA_CH_GATE <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console B Register Out Not Enabled");
+      PS_B_CH_BUS <= "01010101";
+
+      -- Assembly Channel
+
+      PS_CONS_MX_POS_BUS(24) <= '1'; -- identifies character position in stop printout
+      PS_ASSEMBLY_CH_BUS <= tc(6 downto 0);
+      PS_ASSEMBLY_CH_C_CHAR_BIT <= tc(HDL_C_BIT);
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;      
+      checkChar(results,tc,testName,"Console Assembly Channel Out");
+      
+      PS_CONS_MX_POS_BUS(24) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console Assembly Channel Out Not Enabled");
+      PS_ASSEMBLY_CH_BUS <= "1100110";
+
+      -- E2 Register (ordinary I/O)
+
+      PS_CONS_E2_REG_GATE <= '1';
+      PS_E2_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;      
+      checkChar(results,tc,testName,"Console E2 Register Out");
+      
+      PS_CONS_E2_REG_GATE <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console E2 Register Out Not Enabled");
+      PS_E2_REG_BUS <= "11101110";
+      
+      -- The rest do not use the WM bit
+      
+      -- Op Register      
+      
+      PS_CONS_MX_POS_BUS(19) <= '1'; -- identifies character position in stop printout
+      PS_OP_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';      
+      checkChar(results,tc2,testName,"Console OP Register Out");
+      
+      PS_CONS_MX_POS_BUS(19) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console OP Register Out Not Enabled");
+      PS_OP_REG_BUS <= "11001100";
+
+      -- Op Mod Register
+
+      PS_CONS_MX_POS_BUS(20) <= '1';
+      PS_OP_MOD_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;      
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';      
+      checkChar(results,tc2,testName,"Console OP Register Out");
+      
+      PS_CONS_MX_POS_BUS(20) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console OP Register Out Not Enabled");
+      PS_OP_MOD_REG_BUS <= "00110011";
+
+      -- E Ch Unit Select      
+      
+      PS_CONS_MX_POS_BUS(26) <= '1';
+      PS_E_CH_U_SEL_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';                  
+      checkChar(results,tc2,testName,"Console E Ch Unit Select Register Out");
+      
+      PS_CONS_MX_POS_BUS(26) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console E Ch Unit Select Register Out Not Enabled");
+      PS_E_CH_U_SEL_REG_BUS <= "11100111";
+
+      -- F Ch Unit Select      
+      
+      PS_CONS_MX_POS_BUS(28) <= '1';
+      PS_F_CH_U_SEL_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';                  
+      checkChar(results,tc2,testName,"Console E Ch Unit Select Register Out");
+      
+      PS_CONS_MX_POS_BUS(28) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console E Ch Unit Select Register Out Not Enabled");
+      PS_F_CH_U_SEL_REG_BUS <= "11110000";
+
+      -- Unit numbers have no WM, B or A bits
+      
+      -- E Ch Unit Number
+           
+      PS_CONS_MX_POS_BUS(27) <= '1';
+      PS_E_CH_U_NU_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';
+      tc2(HDL_B_BIT) := '0';
+      tc2(HDL_A_BIT) := '0';
+      checkChar(results,tc2,testName,"Console E Ch Unit Select Register Out");
+      
+      PS_CONS_MX_POS_BUS(27) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console E Ch Unit Select Register Out Not Enabled");
+      PS_E_CH_U_NU_REG_BUS <= "10111101";
+
+      -- F Ch Unit Number
+
+      PS_CONS_MX_POS_BUS(29) <= '1';
+      PS_F_CH_U_NU_REG_BUS <= tc;
+      wait for 30 ns;      
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;
+      tc2 := tc;
+      tc2(HDL_WM_BIT) := '0';
+      tc2(HDL_B_BIT) := '0';
+      tc2(HDL_A_BIT) := '0';
+      checkChar(results,tc2,testName,"Console F Ch Unit Select Register Out");
+      
+      PS_CONS_MX_POS_BUS(29) <= '0';
+      wait for 30 ns;  
+      results := PS_CONSOLE_OUTPUT_BUS;
+      results(HDL_WM_BIT) := PS_CONS_OUTPUT_WM_BIT;          
+      checkChar(results,"00000000",testName,"Console F Ch Unit Select Register Out Not Enabled");
+      PS_F_CH_U_NU_REG_BUS <= "01000010";
       
    end loop;
+            
 
    assert false report "Simulation Ended NORMALLY" severity failure;
 
