dcf9d05b3091cbfa6729478eb4c06cbb140b76a9
Jay Jaeger <cube1us@gmail.com>
2020-10-30 20:28:10 +0000

    Page 45.20.08.1 Finished Testing
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@2724 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/ALD_45_20_08_1_CONSOLE_CYCLE_CTRL_MATRIX_OUTPUT_tb.vhdl
+++ VHDL/ALD_45_20_08_1_CONSOLE_CYCLE_CTRL_MATRIX_OUTPUT_tb.vhdl
@@ -173,9 +173,9 @@ uut_process: process
 
    -- Your test bench code
 
-   testName := "15.49.04.1        X";  -- NOTE:  Remove X when editing to set correct length!
+   testName := "45.20.08.1        ";
 
-   for tt in 0 to 2**25 loop
+   for tt in 0 to 2**8 loop
       tv := std_logic_vector(to_unsigned(tt,tv'Length));
       a := tv(0);
       b := tv(1);
@@ -185,26 +185,28 @@ uut_process: process
       f := tv(5);
       g := tv(6);
       h := tv(7);
-      j := tv(8);
-      k := tv(9);
-      l := tv(10);
-      m := tv(11);
-      n := tv(12);
-      o := tv(13);
-      p := tv(14);
-      q := tv(15);
-      r := tv(16);
-      s := tv(17);
-      t := tv(18);
-      u := tv(19);
-      v := tv(20);
-      w := tv(21);
-      x := tv(22);
-      y := tv(23);
-      z := tv(24);
 
+		PS_CONS_MX_Y2_POS <= a;
+		PS_CONS_MX_Y3_POS <= b;
+		PS_CONS_MX_X5_POS <= c;
+		PS_CONS_MX_Y4_POS <= d;
+		PS_CONS_MX_Y1_POS <= e;
+		PS_CONS_MX_Y5_POS <= f;
+		PS_CONS_MX_Y6_POS <= g;
+		PS_CONS_MX_X6_POS <= h;
       
       wait for 30 ns;
+
+		check1(MS_CONS_MX_25_POS,not(e and c),testName,"-S CONS MX 25 POS");
+		check1(PS_CONS_MX_26_POS,a and c,testName,"+S CONS MX 26 POS");
+		check1(PS_CONS_MX_27_POS,b and c,testName,"+S CONS MX 27 POS");
+		check1(PS_CONS_MX_28_POS,d and c,testName,"+S CONS MX 28 POS");
+		check1(MS_CONS_MX_28_POS,not(d and c),testName,"-S CONS MX 28 POS");
+		check1(PS_CONS_MX_29_POS,f and c,testName,"+S CONS MX 29 POS");
+		check1(MS_CONS_MX_29_POS,not(f and c),testName,"-S CONS MX 29 POS");
+		check1(PS_CONS_MX_30_POS,g and c,testName,"+S CONS MX 30 POS");
+		check1(MS_CONS_MX_30_POS,not(g and c),testName,"-S CONS MX 30 POS");
+		check1(MS_CONS_MX_31_POS,not(h and e),testName,"-S CONS MX 31 POS");
       
       
    end loop;
