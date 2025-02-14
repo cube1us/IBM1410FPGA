309e1f676cedf790dfdf6d95c5455ed59fe7bca3
Jay Jaeger <cube1us@gmail.com>
2025-02-14 23:56:46 +0000

    IBM1410_UDP_INPUT_UART_RX.vhdl - Checkpoint commit - maybe end of day
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@3205 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/IBM1410_UDP_INPUT_UART_RX.vhdl
+++ VHDL/IBM1410_UDP_INPUT_UART_RX.vhdl
@@ -37,29 +37,36 @@ entity IBM1410_UDP_INPUT_UART_RX is
           UDP_UART_RESET:               in std_logic;
           -- UDP Side Interface 
           rx_match_cond:                in std_logic;
+          rx_udp_hdr_ready:             out std_logic;
           rx_udp_hdr_valid:             in std_logic;
+          rx_udp_payload_axis_tready:   out std_logic;
           rx_udp_payload_axis_tvalid:   in std_logic;
           rx_udp_payload_axis_tdata:    in std_logic_vector(7 downto 0);
           rx_udp_payload_axis_tlast:    in std_logic;
           -- UART-like Interface
+          UDP_UART_RX_DATA_READY:       in std_logic;
           UDP_UART_RX_DATA_VALID:       out std_logic;
           UDP_UART_RX_BYTE:             out std_logic_vector(7 downto 0);
-          UDP_UART_RX_PACKET_END:       out std_logic;
+          UDP_UART_RX_PACKET_END:       out std_logic
     );
 end IBM1410_UDP_INPUT_UART_RX;
 
 architecture Behavioral of IBM1410_UDP_INPUT_UART_RX is
 
     type rxUdpUartStateType is (UDP_UART_RX_IDLE, UDP_UART_RX_MATCH,
-    UDP_UART_RX_VALID, UDP_UART_RX_DONE);
+    UDP_UART_RX_DONE);
     
-    signal RxUdpUartState: rxUdpUartStateType := UDP_UART_rX_IDLE;
+    signal RxUdpUartState: rxUdpUartStateType := UDP_UART_RX_IDLE;
 
 begin
 
-    txUdpUart: process(FPGA_CLOCK,UDP_UART_RESET,rxUdpUartState,
-        rx_match_cond, rx_udp_hdr_valid, rx_udp_payload_axis_tvalid,
-        rx_udp_payload_axis_tdata, rx_udp_payload_axis_tlast)
+   rx_udp_hdr_ready <= '1' when rxUdpUartState = UDP_UART_RX_IDLE
+      else '0';
+
+    txUdpUart: process(FPGA_CLOCK,UDP_UART_RESET,UDP_UART_RX_DATA_READY,
+        rxUdpUartState,rx_match_cond, rx_udp_hdr_valid,
+        rx_udp_payload_axis_tvalid, rx_udp_payload_axis_tdata, 
+        rx_udp_payload_axis_tlast)
    
     begin
    
@@ -69,7 +76,7 @@ begin
         UDP_UART_RX_DATA_VALID <= '0';
         UDP_UART_RX_BYTE <= "00000000";
         UDP_UART_RX_PACKET_END <= '0';
-        rxUdpUartState <= UDP_UART_TX_IDLE;
+        rxUdpUartState <= UDP_UART_RX_IDLE;
 
     elsif FPGA_CLOCK'event and FPGA_CLOCK = '1' then
 
@@ -86,19 +93,25 @@ begin
             end if;
 
         when UDP_UART_RX_MATCH => 
-            -- Have a match - receive data until we see tlast
-            UDP_UART_RX_DATA_VALID <= rx_udp_payload_axis_tvalid;
-            UDP_UART_RX_PACKET_END <= rx_udp_payload_axis_tlast;
-            if rx_udp_payload_axis_tvalid = '1' then
-                UDP_UART_RX_BYTE <= rx_udp_payload_axis_tdata;
-                if rx_udp_payload_axis_tlast = '1' then
-                    rxUdpUartState <= UDP_UART_RX_DONE;                    
-                else
-                    rxUdpUartState <= UDP_UART_RX_MATCH;
-                end if;
+            rx_udp_payload_axis_tready <= UDP_UART_RX_DATA_READY;
+            if UDP_UART_RX_DATA_READY = '1' then
+               -- Have a match - receive data until we see tlast
+               UDP_UART_RX_DATA_VALID <= rx_udp_payload_axis_tvalid;
+               UDP_UART_RX_PACKET_END <= rx_udp_payload_axis_tlast;
+               if rx_udp_payload_axis_tvalid = '1' then
+                   UDP_UART_RX_BYTE <= rx_udp_payload_axis_tdata;
+                   if rx_udp_payload_axis_tlast = '1' then
+                      rxUdpUartState <= UDP_UART_RX_DONE;                    
+                   else
+                      rxUdpUartState <= UDP_UART_RX_MATCH;
+                   end if;
+               else
+                   rxUdpUartState <= UDP_UART_RX_MATCH;
+               end if;
             else
-                rxUdpUartState <= UDP_UART RX_MATCH;
+               rxUdpUartState <= UDP_UART_RX_MATCH;
             end if;
+            
                 
         when UDP_UART_RX_DONE =>
             -- Stay here until hdr_valid goes false.
