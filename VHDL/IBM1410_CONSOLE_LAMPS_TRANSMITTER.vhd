4f393e7202376de8b1c8b08d56df3f7bc4251f9f
Jay Jaeger <cube1us@gmail.com>
2025-03-20 18:33:37 +0000

    IBM1410_CONSOLE_LAMPS_TRANSMITTER.vhd - Remove forced ID flag, since that will usually now be automatically added by the UDP output subsystem (if previous packet had a flush or was for a different ID).
    git-svn-id: svn://localhost/active/IBM1410FPGA/trunk@3240 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- VHDL/IBM1410_CONSOLE_LAMPS_TRANSMITTER.vhd
+++ VHDL/IBM1410_CONSOLE_LAMPS_TRANSMITTER.vhd
@@ -55,8 +55,8 @@ constant LAMP_SECTIONS: INTEGER := LAMP_VECTOR_BITS / 7;  -- Each lamp section i
 constant COUNTER_MAX: INTEGER := REFRESH_TIME / CLOCKPERIOD;
 
 -- Lamp sync vector - with OUR ID flag stuck on the front.
-constant LAMP_SYNC_COUNT: INTEGER := 6;
-constant LAMP_SYNC_INIT: std_logic_vector((LAMP_SYNC_COUNT * 8)-1 downto 0) := X"3F003F003F81";
+constant LAMP_SYNC_COUNT: INTEGER := 5;
+constant LAMP_SYNC_INIT: std_logic_vector((LAMP_SYNC_COUNT * 8)-1 downto 0) := X"3F003F003F";
 
 type lampState_type is (
    lamp_reset, 
